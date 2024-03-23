1028f2bd991243c0915e9ae7c79fc28445c7f4cc
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2024-03-24 00:30:17 +0530

    create love calculator in python

---
+++ 2.6 Love Calculator.py
@@ -0,0 +1,26 @@
+print("The Love Calculator is calculating your score...")
+name1 = input("What is your name? ") # What is your name?
+name2 = input("What is their name? ") # What is their name?
+
+combined_name = name1.lower() + name2.lower()
+
+true_occurance = 0
+love_occurance = 0
+
+for name_char in combined_name:
+    for true_char in "true":
+        if name_char == true_char:
+            true_occurance += 1
+            
+    for love_char in "love":
+        if name_char == love_char:
+            love_occurance += 1
+            
+score = int(str(true_occurance) + str(love_occurance))
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score >= 40 and score <= 50:
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"You score is {score}.")
